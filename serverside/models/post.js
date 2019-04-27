907eecb577c9fb479a219950fa4511aa492cdd38
Matthew Ray <mattray413@gmail.com>
2019-04-27 12:14:42 -0500

    Delete-Reload

--- serverside/models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-  const postSchema = new mongoose.Schema({
-    txtPost:{ type: String, required: true}
-    
-  });
-  
-module.exports = mongoose.model('post', postSchema,'socialTPost');
